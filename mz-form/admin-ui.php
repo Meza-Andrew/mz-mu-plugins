0d8b03047f4e48b59f8255050db8fbea0ef3ab00
andrew-meza <Andrew.Meza@i-car.com>
2026-03-25 21:50:14 -0400

    Fix Firefox submissions table overflow

--- mz-form/admin-ui.php
+++ mz-form/admin-ui.php
@@ -95,7 +95,7 @@ if (!function_exists('mzf_render_submission_log_inline_styles')) {
         }
 
 $sticky_css = <<<CSS
-.mzf-submissions-table-wrap.meza-admin-table-scroll{
+.mzf-submissions-table-wrap{
     width:100%;
     max-width:100%;
     max-height:calc(100vh - 260px);
@@ -104,21 +104,23 @@ $sticky_css = <<<CSS
     border:1px solid #c3c4c7;
     box-sizing:border-box;
     background:#fff;
+    scroll-behavior:auto;
 }
-.mzf-submissions-table-wrap.meza-admin-table-scroll table.wp-list-table{
-    min-width:max-content;
+.mzf-submissions-table-wrap table.wp-list-table{
+    width:100%;
+    table-layout:fixed;
     border-collapse:separate;
     border-spacing:0;
     border:none !important;
     box-shadow:none !important;
 }
-.mzf-submissions-table-wrap.meza-admin-table-scroll table.wp-list-table thead,
-.mzf-submissions-table-wrap.meza-admin-table-scroll table.wp-list-table tfoot{
+.mzf-submissions-table-wrap table.wp-list-table thead,
+.mzf-submissions-table-wrap table.wp-list-table tfoot{
     position:relative;
     z-index:4;
 }
-.mzf-submissions-table-wrap.meza-admin-table-scroll table.wp-list-table thead th,
-.mzf-submissions-table-wrap.meza-admin-table-scroll table.wp-list-table thead td{
+.mzf-submissions-table-wrap table.wp-list-table thead th,
+.mzf-submissions-table-wrap table.wp-list-table thead td{
     position:sticky;
     top:0;
     z-index:5;
@@ -128,8 +130,8 @@ $sticky_css = <<<CSS
     box-shadow:inset 0 -1px 0 #ccd0d4;
     background-clip:padding-box;
 }
-.mzf-submissions-table-wrap.meza-admin-table-scroll table.wp-list-table tfoot th,
-.mzf-submissions-table-wrap.meza-admin-table-scroll table.wp-list-table tfoot td{
+.mzf-submissions-table-wrap table.wp-list-table tfoot th,
+.mzf-submissions-table-wrap table.wp-list-table tfoot td{
     position:sticky;
     bottom:0;
     z-index:5;
@@ -139,12 +141,12 @@ $sticky_css = <<<CSS
     box-shadow:inset 0 1px 0 #ccd0d4;
     background-clip:padding-box;
 }
-.mzf-submissions-table-wrap.meza-admin-table-scroll table.wp-list-table tbody td{
+.mzf-submissions-table-wrap table.wp-list-table tbody td{
     position:relative;
     z-index:1;
     background-clip:padding-box;
 }
-.mzf-submissions-table-wrap.meza-admin-table-scroll table.wp-list-table thead .check-column{
+.mzf-submissions-table-wrap table.wp-list-table thead .check-column{
     position:sticky;
     top:0;
     left:0;
@@ -156,7 +158,7 @@ $sticky_css = <<<CSS
     box-shadow:inset 0 -1px 0 #ccd0d4;
     background-clip:padding-box;
 }
-.mzf-submissions-table-wrap.meza-admin-table-scroll table.wp-list-table tfoot .check-column{
+.mzf-submissions-table-wrap table.wp-list-table tfoot .check-column{
     position:sticky;
     bottom:0;
     left:0;
@@ -168,7 +170,7 @@ $sticky_css = <<<CSS
     box-shadow:inset 0 1px 0 #ccd0d4;
     background-clip:padding-box;
 }
-.mzf-submissions-table-wrap.meza-admin-table-scroll .widefat .check-column{
+.mzf-submissions-table-wrap .widefat .check-column{
     padding:0;
 }
 CSS;
@@ -1251,31 +1253,31 @@ if (!function_exists('mzf_render_submission_log_admin_page')) {
         echo '<a class="button button-primary" href="' . esc_url($export_url) . '">Export All to CSV</a>';
         echo '</div></div>';
         $table_header_cells = ''
-            . '<th scope="col" class="manage-column column-cb check-column" style="width:32px;min-width:32px;max-width:32px;"><label class="screen-reader-text" for="mzf-select-all-1">Select all submissions</label><input type="checkbox" id="mzf-select-all-1" class="mzf-select-all" aria-label="Select all submissions"></th>'
-            . '<th scope="col" class="manage-column column-mzf_lock" style="width:32px;min-width:32px;max-width:32px;" aria-label="Saved status"></th>'
-            . '<th scope="col" class="manage-column column-mzf_date ' . esc_attr($date_header['class']) . '" style="width:150px;min-width:150px;max-width:150px;">' . $date_header['markup'] . '</th>'
-            . '<th scope="col" class="manage-column column-mzf_name ' . esc_attr($name_header['class']) . '" style="width:175px;min-width:175px;max-width:175px;">' . $name_header['markup'] . '</th>'
-            . '<th scope="col" class="manage-column column-mzf_email" style="width:225px;min-width:225px;max-width:225px;">Email</th>'
-            . '<th scope="col" class="manage-column column-mzf_phone" style="width:125px;min-width:125px;max-width:125px;">Phone</th>'
-            . '<th scope="col" class="manage-column column-mzf_zip" style="width:125px;min-width:125px;max-width:125px;">Zip Code</th>'
-            . '<th scope="col" class="manage-column column-mzf_page" style="width:125px;min-width:125px;max-width:125px;">Page</th>'
-            . '<th scope="col" class="manage-column column-mzf_form" style="width:125px;min-width:125px;max-width:125px;">Form</th>'
-            . '<th scope="col" class="manage-column column-mzf_crm" style="width:125px;min-width:125px;max-width:125px;">CRM Entry</th>'
-            . '<th scope="col" class="manage-column column-mzf_status" style="width:130px;min-width:130px;max-width:130px;">Status</th>';
+            . '<th scope="col" class="manage-column column-cb check-column"><label class="screen-reader-text" for="mzf-select-all-1">Select all submissions</label><input type="checkbox" id="mzf-select-all-1" class="mzf-select-all" aria-label="Select all submissions"></th>'
+            . '<th scope="col" class="manage-column column-mzf_lock" aria-label="Saved status"></th>'
+            . '<th scope="col" class="manage-column column-mzf_date ' . esc_attr($date_header['class']) . '">' . $date_header['markup'] . '</th>'
+            . '<th scope="col" class="manage-column column-mzf_name ' . esc_attr($name_header['class']) . '">' . $name_header['markup'] . '</th>'
+            . '<th scope="col" class="manage-column column-mzf_email">Email</th>'
+            . '<th scope="col" class="manage-column column-mzf_phone">Phone</th>'
+            . '<th scope="col" class="manage-column column-mzf_zip">Zip Code</th>'
+            . '<th scope="col" class="manage-column column-mzf_page">Page</th>'
+            . '<th scope="col" class="manage-column column-mzf_form">Form</th>'
+            . '<th scope="col" class="manage-column column-mzf_crm">CRM Entry</th>'
+            . '<th scope="col" class="manage-column column-mzf_status">Status</th>';
         $table_footer_cells = ''
-            . '<th scope="col" class="manage-column column-cb check-column" style="width:32px;min-width:32px;max-width:32px;"><label class="screen-reader-text" for="mzf-select-all-2">Select all submissions</label><input type="checkbox" id="mzf-select-all-2" class="mzf-select-all" aria-label="Select all submissions"></th>'
-            . '<th scope="col" class="manage-column column-mzf_lock" style="width:32px;min-width:32px;max-width:32px;" aria-label="Saved status"></th>'
-            . '<th scope="col" class="manage-column column-mzf_date ' . esc_attr($date_header['class']) . '" style="width:150px;min-width:150px;max-width:150px;">' . $date_header['markup'] . '</th>'
-            . '<th scope="col" class="manage-column column-mzf_name ' . esc_attr($name_header['class']) . '" style="width:175px;min-width:175px;max-width:175px;">' . $name_header['markup'] . '</th>'
-            . '<th scope="col" class="manage-column column-mzf_email" style="width:225px;min-width:225px;max-width:225px;">Email</th>'
-            . '<th scope="col" class="manage-column column-mzf_phone" style="width:125px;min-width:125px;max-width:125px;">Phone</th>'
-            . '<th scope="col" class="manage-column column-mzf_zip" style="width:125px;min-width:125px;max-width:125px;">Zip Code</th>'
-            . '<th scope="col" class="manage-column column-mzf_page" style="width:125px;min-width:125px;max-width:125px;">Page</th>'
-            . '<th scope="col" class="manage-column column-mzf_form" style="width:125px;min-width:125px;max-width:125px;">Form</th>'
-            . '<th scope="col" class="manage-column column-mzf_crm" style="width:125px;min-width:125px;max-width:125px;">CRM Entry</th>'
-            . '<th scope="col" class="manage-column column-mzf_status" style="width:130px;min-width:130px;max-width:130px;">Status</th>';
+            . '<th scope="col" class="manage-column column-cb check-column"><label class="screen-reader-text" for="mzf-select-all-2">Select all submissions</label><input type="checkbox" id="mzf-select-all-2" class="mzf-select-all" aria-label="Select all submissions"></th>'
+            . '<th scope="col" class="manage-column column-mzf_lock" aria-label="Saved status"></th>'
+            . '<th scope="col" class="manage-column column-mzf_date ' . esc_attr($date_header['class']) . '">' . $date_header['markup'] . '</th>'
+            . '<th scope="col" class="manage-column column-mzf_name ' . esc_attr($name_header['class']) . '">' . $name_header['markup'] . '</th>'
+            . '<th scope="col" class="manage-column column-mzf_email">Email</th>'
+            . '<th scope="col" class="manage-column column-mzf_phone">Phone</th>'
+            . '<th scope="col" class="manage-column column-mzf_zip">Zip Code</th>'
+            . '<th scope="col" class="manage-column column-mzf_page">Page</th>'
+            . '<th scope="col" class="manage-column column-mzf_form">Form</th>'
+            . '<th scope="col" class="manage-column column-mzf_crm">CRM Entry</th>'
+            . '<th scope="col" class="manage-column column-mzf_status">Status</th>';
 
-        echo '<div class="mzf-submissions-table-wrap meza-admin-table-scroll">';
+        echo '<div class="mzf-submissions-table-wrap">';
         echo '<table class="widefat fixed striped table-view-list posts mzf-submissions-table wp-list-table">';
         echo '<thead><tr>' . $table_header_cells . '</tr></thead><tbody>';
 
@@ -1463,7 +1465,7 @@ if (!function_exists('mzf_render_submission_log_admin_page')) {
         echo '</tbody><tfoot><tr>' . $table_footer_cells . '</tr></tfoot></table>';
         echo '</div>';
         echo '</form>';
-        echo '<script>(function(){var toggles=document.querySelectorAll(".mzf-select-all");if(!toggles.length)return;var rows=document.querySelectorAll(".mzf-select-row");var sync=function(checked){for(var i=0;i<toggles.length;i++){toggles[i].checked=checked;}};for(var i=0;i<toggles.length;i++){toggles[i].addEventListener("change",function(){for(var j=0;j<rows.length;j++){rows[j].checked=this.checked;}sync(this.checked);});}for(var k=0;k<rows.length;k++){rows[k].addEventListener("change",function(){var checked=rows.length>0;for(var m=0;m<rows.length;m++){if(!rows[m].checked){checked=false;break;}}sync(checked);});}})();</script>';
+        echo '<script>(function(){var wrap=document.querySelector(".mzf-submissions-table-wrap");if(wrap){wrap.scrollLeft=0;}var toggles=document.querySelectorAll(".mzf-select-all");if(!toggles.length)return;var rows=document.querySelectorAll(".mzf-select-row");var sync=function(checked){for(var i=0;i<toggles.length;i++){toggles[i].checked=checked;}};for(var i=0;i<toggles.length;i++){toggles[i].addEventListener("change",function(){for(var j=0;j<rows.length;j++){rows[j].checked=this.checked;}sync(this.checked);});}for(var k=0;k<rows.length;k++){rows[k].addEventListener("change",function(){var checked=rows.length>0;for(var m=0;m<rows.length;m++){if(!rows[m].checked){checked=false;break;}}sync(checked);});}})();</script>';
 
         if ((int) $query->max_num_pages > 1) {
             $pagination_args = $state_args;
