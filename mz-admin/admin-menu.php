ef7397cbda14f8a51221184538ddec6bb7c595b6
andrew-meza <Andrew.Meza@i-car.com>
2026-04-08 13:46:05 -0400

    Revert "Restore Yoast admin views"
    This reverts commit 80b259e1d9767518dac58af2e898c425418fe1da.

--- mz-admin/admin-menu.php
+++ mz-admin/admin-menu.php
@@ -1449,7 +1449,7 @@ function meza_normalize_admin_plugin_menus(): void
                 continue;
             }
 
-            if (!$is_yoast_menu && str_contains($title, 'yoast')) {
+            if (str_contains($title, 'yoast')) {
                 unset($items[$index]);
                 continue;
             }
@@ -1475,6 +1475,11 @@ function meza_normalize_admin_plugin_menus(): void
             if ($is_redirection) {
                 $item[0] = 'Redirects';
                 if (isset($item[3])) $item[3] = 'Redirects';
+                continue;
+            }
+
+            if ($is_yoast_menu && !in_array($title, ['general', 'settings', 'tools'], true)) {
+                unset($items[$index]);
                 continue;
             }
 
