39f5ce3169b3f7f2cfd946084a87d42c2960e840
andrew-meza <Andrew.Meza@i-car.com>
2026-04-10 16:03:27 -0400

    Revert "Allow user AIOS two-factor access"
    This reverts commit 8bad5bd6500d70678d5148a0b0ea793f64474c45.

--- mz-admin.php
+++ mz-admin.php
@@ -3,7 +3,7 @@
 /**
  * Plugin Name: MZ Admin
  * Description: Admin behavior, editorial workflow, and dashboard customization.
- * Version: 1.1.405
+ * Version: 1.1.404
  * Author: Meza LLC
  * Author URI: https://meza.design
  */
