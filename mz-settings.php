3a3b056ca3b5fbac8f8e74792fa94abdc7ef4319
andrew-meza <Andrew.Meza@i-car.com>
2026-05-03 03:38:21 -0400

    Bump MZ Settings version for cached admin counts

--- mz-settings.php
+++ mz-settings.php
@@ -5,7 +5,7 @@
  * Description: Core site settings, defaults, and bootstrap configuration.
  * Author: Meza LLC
  * Author URI: https://meza.design
- * Version: 1.8.37
+ * Version: 1.8.38
  */
 
 /** ================================
