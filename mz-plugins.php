702fa9c864a91c0ba25e8d6690add3f20a47d552
andrew-meza <Andrew.Meza@i-car.com>
2026-05-05 01:48:54 -0400

    chore(mz-plugins): bump plugin version

--- mz-plugins.php
+++ mz-plugins.php
@@ -3,7 +3,7 @@
 /**
  * Plugin Name: MZ Plugins
  * Description: Environment-based plugin installation, activation, and visibility rules.
- * Version: 1.4.49
+ * Version: 1.4.50
  * Author: Meza LLC
  * Author URI: https://meza.design
  *
